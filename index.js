d2a397d57f4e898b7426de7cc8034ec7570690c7
RishithaGullapalli2705 <143982076+RishithaGullapalli2705@users.noreply.github.com>
2024-02-26 01:50:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const apiEndpoint = 'https://rmiyyswdd2zk3bi2lzzkmerfvm0opoum.lambda-url.us-east-2.on.aws/'; 
 
 
